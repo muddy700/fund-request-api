69137bc3b48d33ddd8dbea950a0115a86477b3eb
muddy700 <mohamedmfaume700@gmail.com>
2022-08-27 16:04:14 +0300

    core: Integrate project with Bugsnag
    For monitoring errors in development and production environments

--- app/Console/Kernel.php
+++ app/Console/Kernel.php
@@ -8,6 +8,17 @@ use Illuminate\Foundation\Console\Kernel as ConsoleKernel;
 class Kernel extends ConsoleKernel
 {
     /**
+     * For Reporting out of memory exceptions to bugsnag.
+     */
+    protected function bootstrappers()
+    {
+        return array_merge(
+            [\Bugsnag\BugsnagLaravel\OomBootstrapper::class],
+            parent::bootstrappers(),
+        );
+    }
+
+    /**
      * Define the application's command schedule.
      *
      * @param  \Illuminate\Console\Scheduling\Schedule  $schedule
@@ -25,7 +36,7 @@ class Kernel extends ConsoleKernel
      */
     protected function commands()
     {
-        $this->load(__DIR__.'/Commands');
+        $this->load(__DIR__ . '/Commands');
 
         require base_path('routes/console.php');
     }
