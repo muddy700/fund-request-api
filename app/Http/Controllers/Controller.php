0eab432f746f3f2e609b601778573e0922816516
muddy700 <mohamedmfaume700@gmail.com>
2022-09-10 14:36:14 +0300

    fix: Remove documentation annotations in Controller

--- app/Http/Controllers/Controller.php
+++ app/Http/Controllers/Controller.php
@@ -7,37 +7,7 @@ use Illuminate\Foundation\Bus\DispatchesJobs;
 use Illuminate\Foundation\Validation\ValidatesRequests;
 use Illuminate\Routing\Controller as BaseController;
 
-/**
- * @OA\Info(
- *      version="1.0.0",
- *      title="Fund Request API",
- *      description="RESTful API Endpoints for iPF-OS (Fund Request Module) ",
- *      @OA\Contact(
- *          email="mohamedmfaume700@gmail.com"
- *      )
- *
- * )
- *
- * @OA\Server(
- *      url=L5_SWAGGER_CONST_HOST,
- *      description="Development API Server"
- * )
-
- *
- *
- */
-
 class Controller extends BaseController
 {
-    /**
-     * @OA\Get(
-     *     path="/api/users",
-     *     @OA\Response(response="200", description="An example endpoint")
-     * )
-     */
-    public function getUsers()
-    {
-    }
-
     use AuthorizesRequests, DispatchesJobs, ValidatesRequests;
 }
