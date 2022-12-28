c295660f31cc001833f524459ea77ec8ce8863d2
muddy700 <mohamedmfaume700@gmail.com>
2022-12-29 00:21:02 +0300

    enhancement: Add function to manage role-permissions
    In role-permission controller

--- app/Http/Controllers/RolePermissionController.php
+++ app/Http/Controllers/RolePermissionController.php
@@ -2,10 +2,12 @@
 
 namespace App\Http\Controllers;
 
+use App\Http\Requests\ManageRolePermissionRequest;
 use App\Http\Requests\StoreRolePermissionRequest;
 use App\Models\Role;
 use App\Models\RolePermission;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\DB;
 
 class RolePermissionController extends Controller
 {
@@ -27,11 +29,16 @@ class RolePermissionController extends Controller
         }
     }
 
-    public function manage(Request $request, $role_id)
+    public function manage(ManageRolePermissionRequest $request, $role_id)
     {
         try {
-            $role = Role::without('rolePermissions')->find($role_id);
+            $flag = (int)$request->flag ?? null;
+            $active_permissions = $request->active_permissions ?? null;
 
+            $added_permissions = $request->added_permissions ?? null;
+            $removed_permissions = $request->removed_permissions ?? null;
+
+            $role = Role::without('rolePermissions')->find($role_id);
             if (!$role) {
                 $error = 'No role found with id: ' . $role_id;
 
@@ -39,22 +46,74 @@ class RolePermissionController extends Controller
             }
 
             /**
-             *
-             * * Approach-1: Delete all existing permissions and create again.
-             * * Approach-2: Send added and removed arrays in request body.
+             *  There are two ways of managing permissoions.
              * 
-             * Delete existing permissions for this role.
+             * * Approach-1: Delete all existing permissions for this role and create again as sent in 'active_permissions' array from request body.
+             * * Approach-2: Remove and add permissions as sent in removed_permissions and added_permissions arrays respectively from request body.
              */
-            RolePermission::where('role_id', $role->id)->delete();
 
-            $new_permissions = $request['permission_ids'] ?? null;
+            if ($flag == 1 && !is_null($active_permissions)) {
+                return $this->useApproachOne($role, $active_permissions);
+            } else if ($flag == 2 && (!is_null($added_permissions) || !is_null($removed_permissions))) {
+                return $this->useApproachTwo($role, $removed_permissions, $added_permissions);
+            } else {
+                $error = 'Oops..!, Something is wrong with your request. Check it and try again buddy!. 😎';
 
-            //Insert new permissions
-            if ($new_permissions && count($new_permissions)) {
-                foreach ($new_permissions as $id) {
+                return $this->sendError('manage', $error, 400, 1);
+            }
+        } catch (\Throwable $th) {
+            return $this->sendError('manage', $th, 500, 1);
+        }
+    }
+
+    public function useApproachOne($role, $active_permissions)
+    {
+        try {
+            if ($active_permissions && count($active_permissions)) {
+                DB::transaction(function () use ($role, $active_permissions) {
+                    // Delete all existing permissions for this role.
+                    RolePermission::where('role_id', $role->id)->delete();
+
+                    // Insert all selected permissions again for this role.
+                    foreach ($active_permissions as $permission_id) {
+                        RolePermission::create([
+                            'role_id' => $role->id,
+                            'permission_id' => $permission_id
+                        ]);
+                    }
+                });
+
+                // Lazy Eager Load relations
+                $role = $role->load('rolePermissions.permission:id,resource,name,description');
+
+                return $this->sendSuccess('managed', $role, 1);
+            } else {
+                // The 'active_permissions' array is empty.
+                $data = ['developerMessage' => 'You did not pass anything in the active_permissions array. 🌚'];
+
+                return $this->sendSuccess('managed', $data, 1);
+            }
+        } catch (\Throwable $th) {
+            return $this->sendError('manage', $th, 500, 1);
+        }
+    }
+
+    public function useApproachTwo($role, $removed_permissions, $added_permissions)
+    {
+        try {
+            if ($removed_permissions && count($removed_permissions)) {
+                // Delete only removed permissions for this role.
+
+                RolePermission::whereIn('permission_id', $removed_permissions)->delete();
+            }
+
+            if ($added_permissions && count($added_permissions)) {
+                // Insert only added permissions for this role.
+
+                foreach ($added_permissions as $permission_id) {
                     RolePermission::create([
                         'role_id' => $role->id,
-                        'permission_id' => $id
+                        'permission_id' => $permission_id
                     ]);
                 }
             }
@@ -67,6 +126,7 @@ class RolePermissionController extends Controller
             return $this->sendError('manage', $th, 500, 1);
         }
     }
+
     // Delete the role-permission with a given id.
     public function destroy($id)
     {
@@ -89,7 +149,7 @@ class RolePermissionController extends Controller
     // Send successful response.
     public function sendError($action, $error, $status = 500, $hasMany = 0)
     {
-        $message = 'Failed to ' . $action . ($hasMany ? ' role-permissions.' : ' role-permission.');
+        $message = 'Failed to ' . $action . ($hasMany ? ' role-permissions.' : ' role-permission.') . ' 😭';
 
         $content = ['message' => $message, 'data' => null, 'error' => $error,];
 
@@ -99,7 +159,7 @@ class RolePermissionController extends Controller
     // Send error response.
     public function sendSuccess($action, $data, $hasMany = 0)
     {
-        $message = ($hasMany ? 'Role-permissions ' : 'Role-permission ') . $action . ' successfully.';
+        $message = ($hasMany ? 'Role-permissions ' : 'Role-permission ') . $action . ' successfully. ✅';
 
         $content = ['message' => $message, 'data' => $data, 'error' => null];
 
